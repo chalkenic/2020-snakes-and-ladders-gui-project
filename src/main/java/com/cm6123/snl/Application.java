5e136663a32cb975fea616b6dc6616bdef5451d6
Carl Jones <jonesc162@cardiff.ac.uk>
2020-03-17 14:50:41 +0000

    Added Code for Assessment 2 starting point.
    Now has a main class that is the entry point for execution.
    
    Also has 2 options for CI.  Students may replace .gitlab-ci.yml with either of the provided templates.  By default, code quality is on.

---
+++ src/main/java/com/cm6123/snl/Application.java
@@ -0,0 +1,24 @@
+package com.cm6123.snl;
+
+/**
+ * Application root.
+ */
+public final class Application {
+
+  /**
+   * Default constructor does nothing.
+   */
+  private Application() {
+    //do nothing
+  }
+
+  /**
+   * Start the application.
+   *
+   * @param args command-line arguments
+   */
+  public static void main(final String[] args) {
+    System.out.println("Welcome to Snakes and Ladders");
+    //TODO
+  }
+}
