f5bb910d4a98c286b0edb00abb2b1eb137ca6bbc
Nick White <whitenj2@cardiff.ac.uk>
2020-04-08 17:27:56 +0100

    After creating function to store current dice roll, tests created & passed for checking if Game getCurrentPlayerRoll variable changes value upon next player's turn. Confirmed working, requesting merge to feature branch.

--- src/test/java/com/cm6123/snl/FeatureWinningSquareTest.java
+++ src/test/java/com/cm6123/snl/FeatureWinningSquareTest.java
@@ -25,12 +25,27 @@ class FeatureWinningSquareTest {
     @Test
     void store_1_player_roll() {
         Game winningGame = new GameBuilder()
-                .withPlayers(2)
+                .withPlayers(1)
                 .withBoardSize(5)
                 .build();
 
 
         winningGame.moveCurrentPlayer(3);
         Assertions.assertEquals(3, winningGame.getCurrentPlayerRoll());
+    }
+
+    @Test
+    void store_multiple_player_rolls() {
+        Game winningGame = new GameBuilder()
+                .withPlayers(2)
+                .withBoardSize(5)
+                .build();
+
+        //Confirms if Game is storing both dice rolls after current player roll but before next player roll.
+        winningGame.moveCurrentPlayer(4);
+        Assertions.assertEquals(4, winningGame.getCurrentPlayerRoll());
+
+        winningGame.moveCurrentPlayer(5);
+        Assertions.assertEquals(5, winningGame.getCurrentPlayerRoll());
     }
 }
