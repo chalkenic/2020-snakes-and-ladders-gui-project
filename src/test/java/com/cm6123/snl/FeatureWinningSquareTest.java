970af3c1442661120cf1183e7915b42a5ff6b759
Nick White <whitenj2@cardiff.ac.uk>
2020-04-08 17:16:26 +0100

    Test created for confirming dice roll of 1 player completing 1 roll on game. Test passed, moving to multiple assertion checks before completing issue.

--- src/test/java/com/cm6123/snl/FeatureWinningSquareTest.java
+++ src/test/java/com/cm6123/snl/FeatureWinningSquareTest.java
@@ -1,12 +1,14 @@
 package com.cm6123.snl;
 
+import com.cm6123.snl.dice.DiceSet;
+import com.cm6123.snl.dice.LoadedDiceFactory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class FeatureWinningSquareTest {
 
     @Test
-    void store_player_position_at_turn_start() {
+    void check_player_position_at_turn_start() {
 
         Game winningGame = new GameBuilder()
                 .withPlayers(2)
@@ -16,6 +18,19 @@ class FeatureWinningSquareTest {
         winningGame.moveCurrentPlayer(5);
         winningGame.moveCurrentPlayer(4);
 
+        //Check's player 1's position on board.
         Assertions.assertEquals(5,winningGame.getCurrentPlayer().getPosition().get());
+    }
+
+    @Test
+    void store_1_player_roll() {
+        Game winningGame = new GameBuilder()
+                .withPlayers(2)
+                .withBoardSize(5)
+                .build();
+
+
+        winningGame.moveCurrentPlayer(3);
+        Assertions.assertEquals(3, winningGame.getCurrentPlayerRoll());
     }
 }
