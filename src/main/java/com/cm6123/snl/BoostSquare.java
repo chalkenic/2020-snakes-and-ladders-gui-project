05530106b95d1ed1e6c44de50a620015a8049d1c
Nick White <whitenj2@cardiff.ac.uk>
2020-04-10 15:19:21 +0100

    Test created to determine Boost's existence in Board upon instantiation. Confirmed exists, issue confirmed.

--- src/main/java/com/cm6123/snl/BoostSquare.java
+++ src/main/java/com/cm6123/snl/BoostSquare.java
@@ -1,17 +1,13 @@
 package com.cm6123.snl;
 
 public class BoostSquare extends Square {
-
-//    private Square destination;
     /**
      * Create a Boost Square on the board, allowing player to move at double their original roll.
      * @param aboard - The board where the boost square resides.
-     * @param index - The position of the Boost square on the board.
-//     * @param boost - Designates s
+     * @param position - The position of the Boost square on the board.
      */
     public BoostSquare(final Board aboard,
-                       final Integer index) {
-        super(aboard, index);
-//        destination = index;
+                       final Integer position) {
+        super(aboard, position);
     }
 }
--- src/test/java/com/cm6123/snl/dice/FeatureBoostSquareTest.java
+++ src/test/java/com/cm6123/snl/dice/FeatureBoostSquareTest.java
@@ -1,11 +1,10 @@
 package com.cm6123.snl.dice;
 
-import com.cm6123.snl.Game;
-import com.cm6123.snl.GameBuilder;
+import com.cm6123.snl.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class FeatureBoostSquareTest {
+class FeatureBoostSquareTest {
 
     @Test
     void store_player_roll() {
@@ -18,5 +17,14 @@ public class FeatureBoostSquareTest {
         boostGame.moveCurrentPlayer(5);
 
         Assertions.assertEquals(5, boostGame.getCurrentPlayerRoll());
+    }
+
+    @Test
+    void create_boost_square() {
+        Board aBoard = new Board(5);
+        Square boost = new BoostSquare(aBoard, 4);
+
+        //Get label sources 1 above position due to zero-indexing.
+        Assertions.assertEquals(5, Integer.parseInt(boost.getLabel()));
     }
 }
