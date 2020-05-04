142254f476ce0f9668b070f6c5ed8f873103904c
Nick White <whitenj2@cardiff.ac.uk>
2020-05-04 15:51:42 +0100

    added 2 blank files for storing newGame & loadGame data.

---
+++ src/main/java/com/cm6123/snl/GUI/Panels/LoadGamePanel.java
@@ -0,0 +1,7 @@
+package com.cm6123.snl.GUI.Panels;
+
+import javax.swing.*;
+
+public class LoadGamePanel extends JPanel {
+
+}
---
+++ src/main/java/com/cm6123/snl/GUI/Panels/NewGamePanel.java
@@ -0,0 +1,8 @@
+package com.cm6123.snl.GUI.Panels;
+
+import javax.swing.*;
+
+public class NewGamePanel extends JPanel {
+
+
+}
