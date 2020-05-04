2512443b6ad77a1a653c16d204a03615b11a326a
Nick White <whitenj2@cardiff.ac.uk>
2020-05-04 20:11:21 +0100

    Added starter variables for NewGamePanel.

--- src/main/java/com/cm6123/snl/GUI/Panels/NewGamePanel.java
+++ src/main/java/com/cm6123/snl/GUI/Panels/NewGamePanel.java
@@ -6,7 +6,31 @@ import javax.swing.*;
 
 public class NewGamePanel extends JPanel {
 
-    public NewGamePanel(GUIFrame gui) {
+    private GUIFrame gameGui;
+    private JLabel playerCountLabel;
+    private JLabel boardSizeLabel;
+    private JLabel snakeChoiceLabel;
+    private JLabel ladderChoiceLabel;
+    private JLabel boostOnlabel;
+    private JLabel boostChoiceLabel;
+    private JLabel winningSquareOnlyOnLabel;
+
+    private JTextField playerCountField;
+    private JSlider boardSizeSlider;
+    private JList snakeChoiceList;
+    private JList ladderChoiceList;
+    //Insert boost tickbox here.
+    private JList boostChoiceList;
+    //Insert winning square tickbox here. 
+
+    public NewGamePanel(final GUIFrame gui) {
+        this.gameGui = gui;
+
+
+
+
+
+
 
     }
 
